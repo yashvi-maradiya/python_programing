59339b4532ea164e96a5dd20fb4c326e911ebdd7
yashvi-maradiya <yashvimaradiya175@gmail.com>
2024-02-07 14:48:30 -0600

    changes

--- day1/operators.py
+++ day1/operators.py
@@ -1,16 +1,18 @@
-#operators
+#Arethmetic operators
+
+#multipication
 23*2
 46
-
+#division
 12/4
 3.0
-
+#modulus
 786%2
 0
-
+#substraction
 345-675
 -330
-
+#addition
 6476+547
 7023
 
@@ -19,5 +21,13 @@
 a=input("enter your first number : ")
 b=input("enter your second number : ")
 sum=int(a) + int(b)
-print(sum)
+print("sum is :",sum)
+print("substracton is :",int(a)-int(b))
+print("multipication is : ",int(a)*int(b))
+print("division is : ",int(a)/int(b))
 
+# exponention(same as multipication)
+print("exponention is : ", int(a)**int(b))
+
+#floor division(round the result down to the nearest whole number)
+print("floor is : ", int(a)//int(b))

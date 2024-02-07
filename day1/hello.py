ff70488c799c56ea9ab5cc1504f359bb78c0877e
yashvi-maradiya <yashvimaradiya175@gmail.com>
2024-02-07 13:12:31 -0600

    operators and hello wrold

---
+++ day1/hello.py
@@ -0,0 +1,3 @@
+#hello world
+Name = input("what is your name ? ")
+print("wellcome to the programing world " , Name)
---
+++ day1/operators.py
@@ -0,0 +1,23 @@
+#operators
+23*2
+46
+
+12/4
+3.0
+
+786%2
+0
+
+345-675
+-330
+
+6476+547
+7023
+
+
+#user must input two number
+a=input("enter your first number : ")
+b=input("enter your second number : ")
+sum=int(a) + int(b)
+print(sum)
+

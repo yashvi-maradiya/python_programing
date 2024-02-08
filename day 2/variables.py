1486b0a26b07ccf3bc7d2261d3be4fea042c4a3a
yashvi-maradiya <yashvimaradiya175@gmail.com>
2024-02-08 15:41:26 -0600

    variable and numaric data type

---
+++ day 2/variables.py
@@ -0,0 +1,25 @@
+# variable
+
+#create a variable called "age" and assing it value 25
+age=25
+print("age = ", age)
+
+#increment age using compound assingment
+age+=1
+print("increment age is : ",age)
+
+#variable = name and assing name as a string
+name="yashvi"
+print(name)
+
+
+#calculate the area of rectangle
+length=5
+width=10
+area=length*width # area formula
+print("area = ",area)
+
+
+#temperature representing in degree celsius
+temp=float(input("Enter a temperature : "))
+print("temperature in celsius : ", temp)

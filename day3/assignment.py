66217013b7bf029b1c03d929e8c8a9780f9e013c
yashvi-maradiya <yashvimaradiya175@gmail.com>
2024-02-12 13:22:59 -0600

    python data types and data structures

--- day3/assignment.py
+++ day3/assignment.py
@@ -1,24 +1,29 @@
 #python data type and structures
 
-# 1. create an integer variable and float variable and add them togather.check out type
+# 1. Create an integer variable and a float variable, and add them together. Check output type
+
 a=int(input("enter your integer variable = "))
 b=float(input("enter your float variable = "))
 print(a+b) #output type is float variable
 
 
-# 2. convert string variable into integer
+# 2. Convert a string variable containing the value "123" to an integer.
+
 x="123"
 print(type(x))
 convert_num=int(x)
 print(type(convert_num))
 print(x)
 
-#3. create a string variable your name and concatenate with string your favorite color
+#3. Create a string variable containing your name, and concatenate it with a string variable containing your favorite color.
+
 name="yashvi"
 color="green"
-print(f'{name} favorite color is {color}') #f-string makes it easy to print values and variables. 
+print(f'{name} favorite color is {color}') #f-string makes it easy to print values and variables.
 
-#4. create a list
+
+#4.  Create a list containing your favorite books
+
 list=["think and grow rich" , "believe in yourself" , "atomic habits" , "gita"]
 print(list)
 print(type(list))
@@ -29,7 +34,8 @@ print(tup)
 print(type(tup))
 
 
-#6. create a dictionary
+#6. Create a dictionary containing the key-value pairs for your name, age, and hometown.
+
 dic={"name" : "yashvi", "age" : 20 , "hometown" : "surat"}
 print(dic)
 print(type(dic))

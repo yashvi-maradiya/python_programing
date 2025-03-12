d45c8f970061439feb27c24af0ca0b4c91ff7623
yashvi-maradiya <yashvimaradiya175@gmail.com>
2025-03-12 15:28:13 +0530

    new code add

--- day 2/numeric_convter.py
+++ day 2/numeric_convter.py
@@ -7,3 +7,12 @@ print("56 binary number is : ",int_to_binary(56))
 print(" 67 binary number is : ",int_to_binary(67))
 
 
+# def int_to_binary(num):
+#     return bin(num)[2:]  # Removing '0b' prefix
+
+# # Taking multiple inputs from the user
+# numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+
+# # Converting each number to binary and printing
+# for num in numbers:
+    # print(f"{num} binary number is: {int_to_binary(num)}")
